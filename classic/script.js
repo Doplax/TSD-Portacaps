d7662aaceb640a72014c551f0fb63a232d81a50d
Doplax <doplax@gmail.com>
2022-10-27 18:02:48 +0200

    Despues de conseguir identificar cual ha sido el checkbox pulsado

--- classic/script.js
+++ classic/script.js
@@ -1,5 +1,5 @@
 const datos = [
-    {id:1,
+    {id:"1",
     company:"Alfreds Futterkiste" , 
     contact:"Maria Anders",  
     country:"Germany"},
@@ -20,13 +20,13 @@ const datos = [
     country:"Germany"}    
 ]
 
-
 // Montando la tabla
 const createTable = (datos) => {
     let table = '<table border="1">' // Opening Table
     //Header
     table += (
         `<tr>
+            <th>Caja</th>
             <th>${Object.keys(datos[0])[1]}</th>
             <th>${Object.keys(datos[0])[2]}</th>
             <th>${Object.keys(datos[0])[3]}</th>
@@ -36,6 +36,7 @@ const createTable = (datos) => {
     datos.forEach((datos) =>{
         table += 
             `<tr>
+                <td> <input type='checkbox' id=${datos.id}></td>
                 <td>${datos.company}</td>
                 <td>${datos.contact}</td>
                 <td>${datos.country}</td>
@@ -46,5 +47,47 @@ const createTable = (datos) => {
 }
 
 createTable(datos)
+
+
+
+// // Lo de los Eventos
+// // window.fncEditCell = function(argThis) {
+// //     alert('Row number of Row Clicked: ' + argThis.parentNode.rowIndex);
+// // };
+
+
+// ZONA DE PRUEBAS
+
+// Para saber  que Checkbox ha sido pulsado
+let input = document.getElementsByTagName('input')
+
+for (let i=0; i < input.length; i++){
+    console.log(input[i].type);
+    input[i].addEventListener("change",(e) => {
+        console.log(e.currentTarget);
+    },false)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
