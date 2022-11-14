0442797b504120fa87be03fd16e6b3385154a83e
Doplax <doplax@gmail.com>
2022-11-14 08:03:15 +0100

    Add: createOptions() en todas las opciones

--- script.js
+++ script.js
@@ -16,45 +16,37 @@ diametros A = {
 }
 */
 
+function createOption(optionValue) {
+    let option = document.createElement("option")
+    option.innerHTML = String(optionValue)
+    diametro.appendChild(option)
+}
+
+
+/////////////////////////////////////////////////
+
+
+
 const diametro = document.querySelector("#options")
 diametro.addEventListener("change", () => {
         if (diametro.value   == 19) {
-
+            createOption(12.7)
                 // cargar 12,7
 
         } else if (diametro.value   == 20){
-                // cargar 15
+            createOption(15)
 
         } else if (diametro.value == 22){
-                // Cargar 15
+            createOption(15)
 
         } else if (diametro.value == 25){
-                // cargar 15, 19
+            createOption(15)
+            createOption(19)
+            // cargar 15, 19
         } 
 })
 
-function prueba() {
-        
-}
 
-function createOption(optionValue) {
-        let option = document.createElement("option")
-        option.innerHTML = String(optionValue)
-        diametro.appendChild(option)
-}
+
 
 //// PRUEBAS
-
-// function Persona(nombre) {
-//         this.nombre = nombre
-// }
-
-let Persona = class {
-        constructor(nombre){
-                this.nombre = "Pol"
-        }
-}
-
-let p = new Persona("oi")
-
-console.log(p);
