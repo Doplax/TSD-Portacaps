aeddd7a43efa6dafd41b4504390e330370d68e0a
Doplax <doplax@gmail.com>
2022-11-23 10:08:18 +0100

    refactor: cambio en la logia de createConicidadOption() 	Ahora se le pueden pasar varios parametros a la vez, para aumentar la escalabilidad

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-/*
-Sustituir L2 y B por "Conicidad"
 
-conicidades {
-    12,7 
-    15 { 20 o}
-    19
+const data = {
+    // diametro 19 { 12,7 }
+    // diametro 20 { 15 }
+    // diametro 22 { 15 }
+    // diametro 25 { 15,19 }
+    19: { 
+        conicidad: 12.7,
+        L: [100, 150, 250]
+    } ,
+    20: {
+        conicidad: 15,
+        L: [80,150,250]
+    },
+    22: {
+        conicidad: 15,
+        L: [80,150,250]
+    },
+    25: {
+        conicidad: [15,19],
+        L: [100,200,325]
+    }
 }
 
-diametros A = {
-    diametro 19 { 12,7}
-    diametro 20 { 15 }
-    diametro 22 { 15 }
-    diametro 25 { 15,19 }
-    
+
+function checkChanges() {
+    if          (diametro.value == 19) {
+        createConicidadOption(12.7)
+
+    } else if (diametro.value == 20){
+        createConicidadOption(15)
+
+    } else if (diametro.value == 22){
+        createConicidadOption(15)
+
+    } else if (diametro.value == 25){
+        createConicidadOption(15,19)
+    } 
+
+    changeReference();
+    loadCotas()
 }
-*/
 
 function changeReference() {
+
     const referencia  = document.getElementById("referencia")
 
     let isDiametroFill = Boolean(diametro.value);
@@ -27,45 +53,28 @@ function changeReference() {
     if (is_opcion_3_Fill && isConicidadFill && isDiametroFill ) {
         referencia.innerHTML = diametro.value + conicidad.value + opcion_3.value
         
-    } else { referencia.innerHTML = ".: REF :."   }
+    } else { referencia.innerHTML = ".: Refer :."   }
     
+}
 
+function createConicidadOption(...optionValue) {
+    clearConicidadList()
 
+    optionValue.forEach((value) => {
+        let newOption = document.createElement("option");
+        newOption.innerHTML = String(value);
+        conicidad.appendChild(newOption);
+    })
+    
 }
 
 function clearConicidadList() {
     while (conicidad.firstChild) { conicidad.removeChild(conicidad.firstChild);} 
 }
 
-function createConicidadOption(optionValue) {
-    let option = document.createElement("option");
-    option.innerHTML = String(optionValue);
-    conicidad.appendChild(option);
+function loadCotas() {
+
 }
-
-function checkChanges() {
-      
-    if          (diametro.value   == 19) {
-        clearConicidadList()
-        createConicidadOption(12.7)
-        
-
-    } else if (diametro.value == 20){
-        clearConicidadList()
-        createConicidadOption(15)
-
-    } else if (diametro.value == 22){
-        clearConicidadList()
-        createConicidadOption(15)
-
-    } else if (diametro.value == 25){
-        clearConicidadList()
-        createConicidadOption(15)
-        createConicidadOption(19)
-    } 
-    changeReference()
-}
-
 
 /////////////////////////////////////////////////
 const diametro = document.querySelector("#diametro")
@@ -78,14 +87,10 @@ const opcion_3 = document.querySelector("#opcion_3")
 opcion_3.addEventListener("click", () => { checkChanges()})
 
 
+let prueba = (...param) => {
+    param.forEach(element => {
+        console.log(element)
+    })
+}
 
-
-
-
-
-
-
-//// PRUEBAS
-
-Math.PI
-console.log(Math.PI);
+prueba("hola","adios")
