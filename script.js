6520cdad252b26bb4217efc3f4661155a9a18ada
Doplax <doplax@gmail.com>
2022-11-24 09:16:06 +0100

    fix: Cambio en el addEventListener de Conicidad y Longitud (Para evitar que cada vez que cambiaras un selector se repitiese el proceso de borrar todo y cargar todo)

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 const listaDiametros = {
     19: { 
-        conicidad: ["-",12.7],
+        conicidad: [12.7],
         L: ["-",100, 150, 250]
     } ,
     20: {
-        conicidad: ["-",15],
+        conicidad: [15],
         L: ["-",80,150,250]
     },
     22: {
-        conicidad: ["-",15],
+        conicidad: [15],
         L: ["-",80,150,250]
     },
     25: {
@@ -18,10 +18,11 @@ const listaDiametros = {
     }
 }
 
-function checkChanges() {
+function checkChangesDiametro() {
     // En vez de escribir un if por cada opcion, podemos hacer esto:
     for ( diametro in listaDiametros){
         if (diametroEscogido.value == diametro) {
+            // Cargar opciones
                 let conicidad = listaDiametros[diametro].conicidad
                 createConicidadOption(conicidad)
 
@@ -30,6 +31,15 @@ function checkChanges() {
         }
     }
 
+    changeReference();
+    loadCotas()
+}
+
+function checkChangesConicidad() {
+    changeReference();
+    loadCotas()
+}
+function checkChangesLongitudL() {
     changeReference();
     loadCotas()
 }
@@ -75,14 +85,13 @@ function clearListOptions(lista) {
 
 function loadCotas() {
     let isDiametroFill = Boolean(diametroEscogido.value);
-    let isLongitudLFill = Boolean(longitudL.value);
 
     if (isDiametroFill) {
         let cota1 = document.querySelector('.cotas p:nth-child(1)')
         cota1.innerHTML = diametroEscogido.value + "mm"
     }
 
-    if (isLongitudLFill) {
+    if (longitudL.value != "-") {
         let cota2 = document.querySelector('.cotas p:nth-child(2)')
         cota2.innerHTML = longitudL.value + "mm"
     }
@@ -90,17 +99,18 @@ function loadCotas() {
 
 /////////////////////////////////////////////////
 const diametroEscogido = document.querySelector("#diametro")
-diametroEscogido.addEventListener("change", () => { checkChanges()})
+diametroEscogido.addEventListener("change", () => { checkChangesDiametro()})
 
 const conicidad = document.querySelector("#conicidad")
-conicidad.addEventListener("change", () => { checkChanges()})
+conicidad.addEventListener("change", () => { checkChangesConicidad()})
 
 const longitudL = document.querySelector("#longitudL")
-longitudL.addEventListener("click", () => { checkChanges()})
+longitudL.addEventListener("click", () => { checkChangesLongitudL()})
+
 
 
 //////////////
 
-console.log();
+console.log("");
 
 
